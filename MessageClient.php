1da742b3a204307a36c39c5e44c740d87430dac2
t3kj3wlz <outlawstar4761@gmail.com>
2019-02-27 09:30:40 -0600

    initial commit

---
+++ MessageClient.php
@@ -0,0 +1,46 @@
+<?php
+
+abstract class MessageClient{
+
+  const AUTHEND = 'http://api.attlocal.net:9667/';
+
+  public static function authenticate($username,$password){
+    $headers = array('request_token: ' . $username,'password: ' . $password);
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,self::AUTHEND . "authenticate");
+    curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+    $output = json_decode(curl_exec($ch));
+    curl_close($ch);
+    if(isset($output->error)){
+      throw new \Exception($output->error);
+    }
+    return $output;
+  }
+  public static function verifyToken($token){
+    $headers = array('auth_token: ' . $token);
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,self::AUTHEND . "verify");
+    curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+    $output = json_decode(curl_exec($ch));
+    curl_close($ch);
+    if(isset($output->error)){
+      throw new \Exception($output->error);
+    }
+    return $output;
+  }
+  public static function send($message,$token){
+    $headers = array('auth_token: ' . $token);
+    $post = "message=" . base64_encode(serialize($message));
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,self::MSGEND . "send");
+    curl_setopt($ch,CURLOPT_POST,1);
+    curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$post);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+  }
+}
