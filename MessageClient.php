69871be4a681f6f86349e98ab37580096cfe0287
t3kj3wlz <outlawstar4761@gmail.com>
2019-02-27 11:50:43 -0600

    added isSent

--- MessageClient.php
+++ MessageClient.php
@@ -2,12 +2,12 @@
 
 abstract class MessageClient{
 
-  const AUTHEND = 'http://api.attlocal.net:9667/';
+  const MSGEND = 'http://api.attlocal.net:9667/';
 
   public static function authenticate($username,$password){
     $headers = array('request_token: ' . $username,'password: ' . $password);
     $ch = curl_init();
-    curl_setopt($ch,CURLOPT_URL,self::AUTHEND . "authenticate");
+    curl_setopt($ch,CURLOPT_URL,self::MSGEND . "authenticate");
     curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
     $output = json_decode(curl_exec($ch));
@@ -20,7 +20,7 @@ abstract class MessageClient{
   public static function verifyToken($token){
     $headers = array('auth_token: ' . $token);
     $ch = curl_init();
-    curl_setopt($ch,CURLOPT_URL,self::AUTHEND . "verify");
+    curl_setopt($ch,CURLOPT_URL,self::MSGEND . "verify");
     curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
     $output = json_decode(curl_exec($ch));
@@ -38,6 +38,16 @@ abstract class MessageClient{
     curl_setopt($ch,CURLOPT_POST,1);
     curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
     curl_setopt($ch,CURLOPT_POSTFIELDS,$post);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+  }
+  public static function isSent($msg_name,$flag,$token){
+    $headers = array('auth_token: ' . $token);
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,self::MSGEND . "sent");
+    curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
     $output = curl_exec($ch);
     curl_close($ch);
